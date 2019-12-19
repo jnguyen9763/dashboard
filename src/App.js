39b3306ddd70bf8d8778eecdd198feabacd7d7c4
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-12-19 00:15:26 -0500

    created dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './components/Nav/Nav'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
   return (
     <div>
       <Nav />
+      <Dashboard />
     </div>
   )
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
 
 function Nav() {
     return (
-        <Navbar bg='dark' variant='dark'>
-            <Navbar.Brand href='/'>
+        <Navbar bg="dark" variant="dark" className="justify-content-between">
+            <Navbar.Brand href="/">
                 <img
-                    src='/images/logo.png'
-                    width='30'
-                    height='30'
-                    className='d-inline-block align-top'
-                    alt='logo'
-                />    
+                    src="/images/logo.png"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="logo"
+                />{" "}    
                 Dashboard
             </Navbar.Brand>
+            <Button type="button" variant="light" className="shadow-none">
+                Add Widget
+            </Button>    
         </Navbar>
     )
 }
