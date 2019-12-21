0ed9a7bb86fa1efb5cb0789361d9826365d85f36
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-12-21 18:06:26 -0500

    added functionality to start, pause, and reset buttons

--- src/components/Pomodoro/Pomodoro.js
+++ src/components/Pomodoro/Pomodoro.js
@@ -3,43 +3,108 @@ import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-pro
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 import Button from 'react-bootstrap/Button'
 import styles from './Pomodoro.module.css'
-import { MdPlayArrow, MdReplay, MdSettings } from 'react-icons/md';
+import { MdPlayArrow, MdReplay, MdSettings, MdPause } from 'react-icons/md'
 
-function Pomodoro() {
-    // const [workDuration, setWorkDuration] = useState(25)
-    // const [breakDuration, setBreakDuration] = useState(5)
+class Pomodoro extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            session: 'Work',
+            clockRunning: false,
+            workDuration: 1500,
+            timeLeft: 1500,
+            breakDuration: 300,
+            ID: 0
+        }
 
-    return (
-        <div className={styles.Pomodoro}>
-            <CircularProgressbarWithChildren
-                className={styles.CircleDisplay}
-                strokeWidth={5}
-                styles={buildStyles({
-                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                    strokeLinecap: 'butt',
-                
-                    // Text size
-                    textSize: '16px',
-                
-                    // How long animation takes to go from one percentage to another, in seconds
-                    pathTransitionDuration: 0.5,
-                
-                    // Colors
-                    pathColor: `white`,
-                    textColor: '#292b2c',
-                    trailColor: 'rgba(255, 255, 255, 0.25)'
-                })}
-            >
-                <div>Work</div>
-                <h3>25:00</h3>
-            </CircularProgressbarWithChildren>
-            <ButtonToolbar className="d-flex justify-content-around">
-                <Button variant="link" size="lg"><MdReplay /></Button>
-                <Button className={styles.playButton} variant="link" size="lg"><MdPlayArrow /></Button>
-                <Button variant="link" size="lg"><MdSettings /></Button>
-            </ButtonToolbar>
-        </div>
-    )
+        this.startClock = this.toggleClock.bind(this, false)
+        this.stopClock = this.toggleClock.bind(this, true)
+    }
+
+    toggleClock(reset) {
+        if (reset) {
+            // stop the timer
+            clearInterval(this.state.ID)
+            this.setState({
+                clockRunning: false,
+                timeLeft: this.state.workDuration,
+                session: 'Work'
+            })
+        } else {
+            if (this.state.clockRunning === true) {
+                // pause the timer
+                clearInterval(this.state.ID)
+                this.setState({clockRunning: false})
+            } else {
+                // start the timer
+                const ID = setInterval(() => this.toggleSession(), 1000)
+                this.setState({ID: ID, clockRunning: true})
+            }
+        }
+    }
+
+    toggleSession() {
+        if (this.state.timeLeft > 0) {
+            // decrease time left
+            let time = this.state.timeLeft
+            this.setState({ timeLeft: --time })
+        } else if (this.state.timeLeft === 0) {
+            // timer is over -> if work switch to break, viceversa
+            if (this.state.session === 'Work') {
+                this.setState({
+                    session: 'Break',
+                    timeLeft: this.state.breakDuration
+                })
+            } else {
+                this.setState({
+                    session: 'Work',
+                    timeLeft: this.state.workDuration
+                })
+            }
+        }
+    }
+
+    displayTime() {
+        const minutes = Math.floor(this.state.timeLeft / 60)
+        const seconds = Math.floor(this.state.timeLeft % 60)
+        return ('0' + minutes).slice(-2) + " : " + ('0' + seconds).slice(-2)
+    }
+
+    render() {
+        return (
+            <div className={styles.Pomodoro}>
+                <CircularProgressbarWithChildren
+                    className={styles.CircleDisplay}
+                    strokeWidth={5}
+                    styles={buildStyles({
+                        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                        strokeLinecap: 'butt',
+                    
+                        // Text size
+                        textSize: '16px',
+                    
+                        // How long animation takes to go from one percentage to another, in seconds
+                        pathTransitionDuration: 0.5,
+                    
+                        // Colors
+                        pathColor: `white`,
+                        textColor: '#292b2c',
+                        trailColor: 'rgba(255, 255, 255, 0.25)'
+                    })}
+                >
+                    <div>{this.state.session}</div>
+                    <h3>{this.displayTime()}</h3>
+                </CircularProgressbarWithChildren>
+                <ButtonToolbar className="d-flex justify-content-around">
+                    <Button onClick={this.stopClock} variant="link" size="lg"><MdReplay /></Button>
+                    {!this.state.clockRunning ? 
+                    <Button ref="start" onClick={this.startClock} className={styles.playButton} variant="link" size="lg"><MdPlayArrow /></Button> :
+                    <Button ref="start" onClick={this.startClock} className={styles.playButton} variant="link" size="lg"><MdPause /></Button>}
+                    <Button variant="link" size="lg"><MdSettings /></Button>
+                </ButtonToolbar>
+            </div>
+        )
+    }
 }
 
 export default Pomodoro
