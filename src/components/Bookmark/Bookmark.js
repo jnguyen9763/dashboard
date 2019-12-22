4b177bc7cfa7f4c20ef1b00696a1245e758788d5
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-12-21 22:41:03 -0500

    created very basic version of a bookmark

---
+++ src/components/Bookmark/Bookmark.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import styles from './Bookmark.module.css'
+import Button from 'react-bootstrap/Button'
+import { MdAdd } from 'react-icons/md'
+
+function Bookmark() {
+    const [link, setLink] = useState('')
+
+    const openLink = () => {
+        if (link === '') {
+            const link = prompt('Enter a link you would like to bookmark')
+            setLink(link)
+        }
+        else {
+            window.open(link, '_blank')
+        }
+    }
+
+    return (
+        <div className="d-flex justify-content-center">
+            {link === '' ? 
+            <Button onClick={openLink} className={styles.Bookmark}>
+                <MdAdd />
+            </Button> :
+            <Button onClick={openLink} className={styles.Link}>
+                <img src={link + '/favicon.ico'} alt={link} />
+            </Button>}
+        </div>
+    )
+}
+
+export default Bookmark
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,6 +9,7 @@ import AnalogClock from '../Clock/AnalogClock'
 import DateDisplay from '../Date/Date'
 import Weather from '../Weather/Weather'
 import Pomodoro from '../Pomodoro/Pomodoro'
+import Bookmark from '../Bookmark/Bookmark'
 
 const ReactGridLayout = WidthProvider(RGL)
 const originalLayout = getFromLS("layout") || []
@@ -19,109 +20,113 @@ const size = window.innerWidth / columns
  * This layout demonstrates how to sync to localstorage.
  */
 class Dashboard extends React.PureComponent {
-  static defaultProps = {
-    className: "layout",
-    margin: [0, 0],
-    cols: columns,
-    rowHeight: size,
-    compactType: null,
-    preventCollision: true,
-    isResizable: false,
-    isDraggable: false,
-    onLayoutChange: function() {}
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      layout: JSON.parse(JSON.stringify(originalLayout)),
-      date: new Date()
+    static defaultProps = {
+        className: "layout",
+        margin: [0, 0],
+        cols: columns,
+        rowHeight: size,
+        compactType: null,
+        preventCollision: true,
+        isResizable: false,
+        isDraggable: false,
+        onLayoutChange: function() {}
     }
 
-    this.onLayoutChange = this.onLayoutChange.bind(this)
-  }
+    constructor(props) {
+        super(props)
 
-  componentDidMount() {
-    // clock and date
-    setInterval(() => this.setState({ date: new Date() }), 1000);
-  }
+        this.state = {
+        layout: JSON.parse(JSON.stringify(originalLayout)),
+        date: new Date()
+        }
 
-  onLayoutChange(layout) {
-    /*eslint no-console: 0*/
-    saveToLS("layout", layout)
-    this.setState({ layout })
-    this.props.onLayoutChange(layout) // updates status display
-  }
+        this.onLayoutChange = this.onLayoutChange.bind(this)
+    }
 
-  render() {
-    return (
-        <ReactGridLayout
-          {...this.props}
-          layout={this.state.layout}
-          onLayoutChange={this.onLayoutChange}
-        >
-            <div className={styles.Test} key="searchbar" data-grid={{x: 0, y: 0, w: 24, h: 2}}>
-                <Searchbar />
-            </div>
+    componentDidMount() {
+        // clock and date
+        setInterval(() => this.setState({ date: new Date() }), 1000);
+    }
 
-            <div className={styles.Test} key="quote" data-grid={{x: 24, y: 0, w: 26, h: 3}}>
-                <Quote />
-            </div>
+    onLayoutChange(layout) {
+        /*eslint no-console: 0*/
+        saveToLS("layout", layout)
+        this.setState({ layout })
+        this.props.onLayoutChange(layout) // updates status display
+    }
 
-            <div className={styles.Test} key="digitalClock12" data-grid={{x: 0, y: 2, w: 6, h: 6}}>
-                <DigitalClock time={this.state.date} hours24={false} />
-            </div>
+    render() {
+        return (
+            <ReactGridLayout
+            {...this.props}
+            layout={this.state.layout}
+            onLayoutChange={this.onLayoutChange}
+            >
+                <div className={styles.Test} key="searchbar" data-grid={{x: 0, y: 0, w: 24, h: 2}}>
+                    <Searchbar />
+                </div>
 
-            <div className={styles.Test} key="digitalClock24" data-grid={{x: 6, y: 2, w: 6, h: 6}}>
-                <DigitalClock time={this.state.date} hours24={true} />
-            </div>
+                <div className={styles.Test} key="quote" data-grid={{x: 24, y: 0, w: 26, h: 3}}>
+                    <Quote />
+                </div>
 
-            <div className={styles.Test} key="analogClock" data-grid={{x: 12, y: 2, w: 6, h: 6}}>
-                <AnalogClock time={this.state.date} renderNumbers={false} />
-            </div>
+                <div className={styles.Test} key="digitalClock12" data-grid={{x: 0, y: 2, w: 6, h: 6}}>
+                    <DigitalClock time={this.state.date} hours24={false} />
+                </div>
 
-            <div className={styles.Test} key="analogClockNums" data-grid={{x: 18, y: 2, w: 6, h: 6}}>
-                <AnalogClock time={this.state.date} renderNumbers={true} />
-            </div>
+                <div className={styles.Test} key="digitalClock24" data-grid={{x: 6, y: 2, w: 6, h: 6}}>
+                    <DigitalClock time={this.state.date} hours24={true} />
+                </div>
 
-            <div className={styles.Test} key="date" data-grid={{x: 24, y: 2, w: 6, h: 6}}>
-                <DateDisplay date={this.state.date} />
-            </div>
+                <div className={styles.Test} key="analogClock" data-grid={{x: 12, y: 2, w: 6, h: 6}}>
+                    <AnalogClock time={this.state.date} renderNumbers={false} />
+                </div>
 
-            <div className={styles.Test} key="weather" data-grid={{x: 30, y: 2, w: 12, h: 7}}>
-                <Weather />
-            </div>
+                <div className={styles.Test} key="analogClockNums" data-grid={{x: 18, y: 2, w: 6, h: 6}}>
+                    <AnalogClock time={this.state.date} renderNumbers={true} />
+                </div>
 
-            <div className={styles.Test} key="pomodoro" data-grid={{x: 42, y: 2, w: 6, h: 9}}>
-                <Pomodoro />
-            </div>
-        </ReactGridLayout>
-    )
-  }
+                <div className={styles.Test} key="date" data-grid={{x: 24, y: 2, w: 6, h: 6}}>
+                    <DateDisplay date={this.state.date} />
+                </div>
+
+                <div className={styles.Test} key="weather" data-grid={{x: 30, y: 2, w: 12, h: 7}}>
+                    <Weather />
+                </div>
+
+                <div className={styles.Test} key="pomodoro" data-grid={{x: 42, y: 2, w: 6, h: 9}}>
+                    <Pomodoro />
+                </div>
+
+                <div className={styles.Test} key="bookmark" data-grid={{x: 48, y: 2, w: 2, h: 2}}>
+                    <Bookmark />
+                </div>
+            </ReactGridLayout>
+        )
+    }
 }
 
 function getFromLS(key) {
-  let ls = {}
-  if (global.localStorage) {
-    try {
-      ls = JSON.parse(global.localStorage.getItem("dashboard")) || {}
-    } catch (e) {
-      /*Ignore*/
+    let ls = {}
+    if (global.localStorage) {
+        try {
+            ls = JSON.parse(global.localStorage.getItem("dashboard")) || {}
+        } catch (e) {
+            /*Ignore*/
+        }
     }
-  }
-  return ls[key]
+    return ls[key]
 }
 
 function saveToLS(key, value) {
-  if (global.localStorage) {
-    global.localStorage.setItem(
-      "dashboard",
-      JSON.stringify({
-        [key]: value
-      })
-    )
-  }
+    if (global.localStorage) {
+        global.localStorage.setItem(
+            "dashboard",
+            JSON.stringify({
+                [key]: value
+            })
+        )
+    }
 }
 
 export default Dashboard
