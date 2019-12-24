f13aa1f794d5281e2e23090c0a134c781c90125b
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-12-24 15:18:40 -0500

    implemented operations: +, -, /, *

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -8,25 +8,52 @@ import { MdBackspace } from 'react-icons/md'
 
 function Calculator() {
     const [number, setNumber] = useState('')
+    const [operatedNumber, setOperatedNumber] = useState('')
     const [hasDecimal, setHasDecimal] = useState(false)
+    const [operation, setOperation] = useState('')
 
     const addNum = num => {
-        let currNum = (' ' + number).slice(1);
         if (hasDecimal && num === '.') return
         if (!hasDecimal && num === '.') {
             setHasDecimal(true)
-            if (currNum === '') setNumber('0.')
-            else setNumber(currNum + num)
+            if (number === '') setNumber('0.')
+            else setNumber(number + num)
             return
         }
-        setNumber(currNum + num)
+        setNumber(number + num)
+    }
+
+    const operate = operation => {
+        const num = parseFloat(number)
+        setOperation(operation)
+        setNumber('')
+        if (operatedNumber === '') {
+            setOperatedNumber(num)
+            return
+        }
+        switch(operation) {
+            case '+':
+                setOperatedNumber(operatedNumber + num)
+                break;
+            case '-':
+                setOperatedNumber(operatedNumber - num)
+                break;
+            case '/':
+                setOperatedNumber(operatedNumber / num)
+                break;
+            case '*':
+                setOperatedNumber(operatedNumber * num)
+                break;
+            default:
+                break;
+        }
     }
 
     return (
         <div className={styles.Calculator}>
             <Container>
                 <Row>
-                    <h6>test</h6>
+                    <h6>{operatedNumber === 0 ? '' : operatedNumber}</h6>
                 </Row>
                 <Row>
                     <h4>{number}</h4>
@@ -34,25 +61,25 @@ function Calculator() {
                 <Row>
                     <Button variant="link" className={styles.Button} as={Col} sm={6}>AC</Button>
                     <Button variant="link" className={styles.Button} as={Col}><MdBackspace /></Button>
-                    <Button variant="link" className={styles.Button} as={Col}>÷</Button>
+                    <Button variant="link" className={styles.Button} as={Col} onClick={() => operate('/')}>÷</Button>
                 </Row>
                 <Row>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('1')}>1</Button>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('2')}>2</Button>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('3')}>3</Button>
-                    <Button variant="link" className={styles.Button} as={Col}>×</Button>
+                    <Button variant="link" className={styles.Button} as={Col} onClick={() => operate('*')}>×</Button>
                 </Row>
                 <Row>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('4')}>4</Button>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('5')}>5</Button>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('6')}>6</Button>
-                    <Button variant="link" className={styles.Button} as={Col}>+</Button>
+                    <Button variant="link" className={styles.Button} as={Col} onClick={() => operate('+')}>+</Button>
                 </Row>
                 <Row>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('7')}>7</Button>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('8')}>8</Button>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('9')}>9</Button>
-                    <Button variant="link" className={styles.Button} as={Col}>-</Button>
+                    <Button variant="link" className={styles.Button} as={Col} onClick={() => operate('-')}>-</Button>
                 </Row>
                 <Row>
                     <Button variant="link" className={styles.Button} as={Col} onClick={() => addNum('.')}>.</Button>
