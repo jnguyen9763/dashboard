3fb5a5dbd1541a9ac05d26f08be16a905d8e8c7f
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-12-27 23:06:23 -0500

    readded yahoo and bing search options

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -3,21 +3,67 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import styles from './Searchbar.module.css'
 import { MdSearch } from 'react-icons/md'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 import googleLogo from '../../assets/images/google.png'
+import yahooLogo from '../../assets/images/yahoo.png'
+import bingLogo from '../../assets/images/bing.png'
 
 function Searchbar() {
     const [input, setInput] = useState('')
+    const [searchEngine, setSearchEngine] = useState("Google")
+    const [searchEngineIcon, setSearchEngineIcon] = useState(googleLogo)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        let url = 'http://google.com/search?q='
+        let url = '';
+        switch(searchEngine) {
+            case "Google":
+                url += 'http://google.com/search?q='
+                break;
+            case "Yahoo":
+                url += 'http://search.yahoo.com/search?q='
+                break;
+            case "Bing":
+                url += 'http://bing.com/search?q='
+                break;
+            default:
+                url += 'http://google.com/search?q='
+        }
         window.open(url + input, '_blank')
         setInput('')
     }
 
+    const changeSearchEngine = (e) => {
+        setSearchEngine(e)
+        switch(e) {
+            case "Google":
+                setSearchEngineIcon(googleLogo)
+                break;
+            case "Yahoo":
+                setSearchEngineIcon(yahooLogo)
+                break;
+            case "Bing":
+                setSearchEngineIcon(bingLogo)
+                break;
+            default:
+                setSearchEngineIcon(googleLogo)
+        }
+    }
+
     return (
         <Form onSubmit={handleSubmit} className={styles.Searchbar}>
-            <img src={googleLogo} alt="google" />
+            <img src={searchEngineIcon} alt="google" />
+            <DropdownButton
+                id="searchEngineDropdown"
+                title=""
+                variant="link"
+                className={styles.Dropdown}
+            >
+                <Dropdown.Item onSelect={changeSearchEngine} eventKey="Google">Google</Dropdown.Item>
+                <Dropdown.Item onSelect={changeSearchEngine} eventKey="Yahoo">Yahoo</Dropdown.Item>
+                <Dropdown.Item onSelect={changeSearchEngine} eventKey="Bing">Bing</Dropdown.Item>
+            </DropdownButton>
             <Form.Control 
                 type="text" 
                 value={input} 
