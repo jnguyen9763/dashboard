1b86797ffee8c398a2008f8b1654ac7ec0c49440
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-12-29 19:16:49 -0500

    Updates

---
+++ precache-manifest.cbe16d4210beeb0ade90bd634ba5f896.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3267b5778b7db6eb6ef02a7fee475bcf",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "6ae05681201c3771f5e6",
+    "url": "/dashboard/static/css/2.a97c96f3.chunk.css"
+  },
+  {
+    "revision": "0df2835e067a3adac6b2",
+    "url": "/dashboard/static/css/main.6eeed3a7.chunk.css"
+  },
+  {
+    "revision": "6ae05681201c3771f5e6",
+    "url": "/dashboard/static/js/2.0b2e2ac0.chunk.js"
+  },
+  {
+    "revision": "9ff0dcd19fa01f6381d9a4a3e60505af",
+    "url": "/dashboard/static/js/2.0b2e2ac0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0df2835e067a3adac6b2",
+    "url": "/dashboard/static/js/main.a391f2e8.chunk.js"
+  },
+  {
+    "revision": "1d1fc3f9498a998154b7",
+    "url": "/dashboard/static/js/runtime-main.f1fa6400.js"
+  },
+  {
+    "revision": "440a3c7f301479d4d4ec6482e9d9d487",
+    "url": "/dashboard/static/media/adding-widget.440a3c7f.mp4"
+  },
+  {
+    "revision": "cdd3ef845a2bd9cef5cee914d92a0163",
+    "url": "/dashboard/static/media/moving-widget.cdd3ef84.mp4"
+  },
+  {
+    "revision": "362af86ef6397007ca30c71ffaeafc19",
+    "url": "/dashboard/static/media/removing-widget.362af86e.mp4"
+  },
+  {
+    "revision": "159a107c9bb4f4988026978fb0b12d92",
+    "url": "/dashboard/static/media/weather-widget.159a107c.mp4"
+  },
+  {
+    "revision": "1d4129887c53d9b274426268bc460106",
+    "url": "/dashboard/static/media/widget-settings.1d412988.mp4"
+  }
+]);
